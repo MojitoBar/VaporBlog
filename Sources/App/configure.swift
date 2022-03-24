d9663c6a2c49c58b755caea9e6aac9b52adbe675
MojitoBar <wnehdtjr5@naver.com>
2022-03-25 04:47:11 +0900

    path change

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,7 +5,7 @@ import LeafMarkdown
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    app.directory.publicDirectory = "/Users/judongseok/Desktop/SwiftProject/BlogVapor/hello/Public"
+//    app.directory.publicDirectory = "/Users/judongseok/Desktop/SwiftProject/BlogVapor/hello/Public"
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     // register Leaf - leaf 등록
@@ -14,7 +14,7 @@ public func configure(_ app: Application) throws {
     app.leaf.cache.isEnabled = app.environment.isRelease
     app.leaf.tags["markdown"] = Markdown()
     // 경로 설정 변경
-    app.leaf.configuration.rootDirectory = "/Users/judongseok/Desktop/SwiftProject/BlogVapor/hello/Resources/Views"
+//    app.leaf.configuration.rootDirectory = "/Users/judongseok/Desktop/SwiftProject/BlogVapor/hello/Resources/Views"
     
     // register routes
     try routes(app)
