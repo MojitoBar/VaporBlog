c54e138453542c32de47079b1dfca48cf163daf4
MojitoBar <wnehdtjr5@gmail.com>
2022-05-23 14:04:38 +0900

    feat: loadview post

--- Sources/App/postData.swift
+++ Sources/App/postData.swift
@@ -5,6 +5,13 @@
 //  Created by judongseok on 2022/03/21.
 //
 let postDatas: [IndexpostInfo] = [
+    IndexpostInfo(title: "loadView와 viewDidLoad의 차이",
+    context: "여느때와 다름없이 깃헙으로 다른 사람들의 소스를 염탐하던 중 loadView()에서 커스텀 뷰를 생성하는 코드를 보게 되었습니다. 평소에 viewDidLoad에서 UI를 그리고 추가하는 코드를 작성하던 저는 loadView와 viewDidLoad의 차이점에 대해 궁금해졌습니다.",
+    date: "2022-05-22",
+    file: "LoadView",
+    tags: [.Swift, .iOS],
+    thumb: "/images/vclifecycle.jpg"),
+    
     IndexpostInfo(title: "2022 카카오 인턴 코딩테스트 & 2022 프로그래머스 섬머코딩 후기",
     context: "알고리즘을 놓지않고 매주 푼 성과를 보기위해 5월 7일 카카오 인턴쉽, 8일 프로그래머스 섬머코딩에 지원했습니다. 이번 포스팅에서는 각 코딩테스트의 후기를 작성해보려합니다. 그리고 다음부턴 절대 이틀 연속으로 코딩테스트를 잡지 말아야겠습니다 일주일에 하루정도는 쉬기위해서...",
     date: "2022-05-11",
